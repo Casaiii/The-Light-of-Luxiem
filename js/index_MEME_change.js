0bd32b431733893ff5cb106c12608418bc67ac8a
cassey <cassey10190@gmail.com>
2022-05-28 23:35:50 +0800

    0527_index_fanart_pc

--- js/index_MEME_change.js
+++ js/index_MEME_change.js
@@ -1,21 +1,35 @@
 //-----------MEME 圖替換-----------
 
 $(".MEME_btn_1").click(function () {
-    $(".index_MEME_img img").attr("src", $(".index_MEME_img_1 img").attr("src"));
-})
+  $(".index_MEME_img img").attr("src", $(".index_MEME_img_1 img").attr("src"));
+});
 
 $(".MEME_btn_2").click(function () {
-    $(".index_MEME_img img").attr("src", $(".index_MEME_img_2 img").attr("src"));
-})
+  $(".index_MEME_img img").attr("src", $(".index_MEME_img_2 img").attr("src"));
+});
 
 $(".MEME_btn_3").click(function () {
-    $(".index_MEME_img img").attr("src", $(".index_MEME_img_3 img").attr("src"));
-})
+  $(".index_MEME_img img").attr("src", $(".index_MEME_img_3 img").attr("src"));
+});
 
 $(".MEME_btn_4").click(function () {
-    $(".index_MEME_img img").attr("src", $(".index_MEME_img_4 img").attr("src"));
-})
+  $(".index_MEME_img img").attr("src", $(".index_MEME_img_4 img").attr("src"));
+});
 
 $(".MEME_btn_5").click(function () {
-    $(".index_MEME_img img").attr("src", $(".index_MEME_img_5 img").attr("src"));
-})
+  $(".index_MEME_img img").attr("src", $(".index_MEME_img_5 img").attr("src"));
+});
+
+//----------------創作區切換-------------------
+
+$(function () {
+  var $li = $("ul.tab-title li");
+  $($li.eq(0).addClass("active").find("a").attr("href"))
+    .siblings(".tab-inner")
+    .hide();
+
+  $li.click(function () {
+    $($(this).find("a").attr("href")).show().siblings(".tab-inner").hide();
+    $(this).addClass("active").siblings(".active").removeClass("active");
+  });
+});
