78b658e484dcebde083394716e45b9fd17f55a08
cassey <cassey10190@gmail.com>
2022-06-03 16:10:03 +0800

    0603_index_imgnewpackage

--- js/index_intro_Bigtrain.js
+++ js/index_intro_Bigtrain.js
@@ -1,18 +1,20 @@
-var step = 0;
-let trainliWidth = $(".index_intro_Ike_outwrap").innerWidth();
+let step = 0;
+const trainliWidth = $(".index_intro_Ike_outwrap").innerWidth();
+console.log(trainliWidth, "trainliWidth");
 
-$(".index_intro_chevron_right_wrap svg").click(function () {
+$(".index_intro_chevron_right_wrap").click(function () {
+  console.log("+1", step);
   step++;
   if (step >= 5) {
     step = 0;
   }
+  $(".numerator").text(step + 1);
 
   stepUpdate();
-
-  $(".numerator").text(step + 1);
 });
 
-$(".index_intro_chevron_left_wrap svg").click(function () {
+$(".index_intro_chevron_left_wrap").click(function () {
+  console.log("-1");
   step--;
   if (step <= -1) {
     step = 4;
@@ -29,3 +31,9 @@ function stepUpdate() {
     `translateX(${step * -trainliWidth}px)`
   );
 }
+
+$(window).resize(function () {
+  trainliWidth = $(".index_intro_Ike_outwrap").innerWidth();
+  stepUpdate();
+  console.log(trainliWidth, "trainliWidth");
+});
